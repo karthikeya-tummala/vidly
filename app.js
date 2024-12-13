124a356dacaa0941b0031f5f17ba505e9d301dd1
karthikeya-tummala <karthikeyatummala@gmail.com>
2024-12-13 21:55:43 +0530

    Added rentals api

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const genres = require('./routes/genres');
 const home = require('./routes/home');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
+const rentals = require('./routes/rentals')
 
 app.use(express.json());
 
@@ -12,6 +13,7 @@ app.use('/', home);
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => { console.log(`Listening of port ${port}!!!`) });
