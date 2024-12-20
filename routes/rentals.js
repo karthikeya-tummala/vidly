3dc55c71669a4a21038644328611966efa1a90a5
karthikeya-tummala <karthikeyatummala@gmail.com>
2024-12-20 10:21:44 +0530

    Added routes for GET and POST for rentals

--- routes/rentals.js
+++ routes/rentals.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const { Transaction } = require('mongoose-transactions');
 const { Rental, validate } = require('../models/rental');
 const { Movie } = require('../models/movie');
 const { Customer } = require('../models/customer');
 const router = express.Router();
-
-
 
 router.get('/', async (req, res) => {
     const rentals = await Rental.find().sort('-dateOut');
@@ -18,11 +17,10 @@ router.post('/', async (req, res) => {
     const customer = await Customer.findById(req.body.customerId);
     if(!customer) return res.status(400).send('Invalid Customer');
 
-
     const movie = await Movie.findById(req.body.movieId);
     if(!movie) return res.status(400).send('Invalid Movie');
 
-    if(movie.numberInStock === 0) return res.status(400).send('Movie not in stock.')
+    if(movie.numberInStock === 0) return res.status(400).send('Movie not in stock.');
 
     let rental = new Rental({
         customer: {
@@ -35,14 +33,21 @@ router.post('/', async (req, res) => {
             title: movie.title,
             dailyRentalRate: movie.dailyRentalRate
         }
-    })
+    });
 
-    rental = await rental.save();
-
-    movie.numberInStock--;
-    movie.save();
-
-    res.send(rental);
+    const transaction = new Transaction();
+    try{
+        await transaction.start();
+        rental = await transaction.insert(rental);
+        await transaction.update(Movie, movie._id, { $inc: { numberInStock: -1 } }, {conditions: {numberInStock: {$gt: 0}}});
+        await transaction.commit();
+        res.send(rental);
+    }
+    catch (err) {
+        await transaction.rollback();
+        transaction.clean();
+        res.status(500).send('Something went wrong while processing the rental.');
+    }
 });
 
 module.exports = router;
