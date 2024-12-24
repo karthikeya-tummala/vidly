46f186658a4cec801390ae68bb499c3735673ab9
karthikeya-tummala <karthikeyatummala@gmail.com>
2024-12-24 15:04:17 +0530

    Added authentication for users

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const home = require('./routes/home');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
-const users = require('./routes/users')
+const users = require('./routes/users');
+const auth = require('./routes/auth');
 
 app.use(express.json());
 
@@ -16,6 +17,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => { console.log(`Listening of port ${port}!!!`) });
---
+++ routes/auth.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const argon2 = require('argon2');
+const Joi = require('joi');
+const { User } = require('../models/user');
+
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    const user = await User.findOne({email: req.body.email});
+    if(!user) return res.status(400).send('Invalid email or password');
+
+    try{
+        const isMatch = await argon2.verify(user.password, req.body.password);
+        if(!isMatch) return res.status(400).send('Invalid email or password');
+
+        res.send(true);
+
+    } catch (err) {
+        console.error('Error verifying password:', err);
+        res.status(500).send('Internal server error');
+    }
+});
+
+function validate(req) {
+    const schema = Joi.object({
+        email: Joi.string().required().min(5).max(255).email(),
+        password: Joi.string().required().min(4).max(255),
+    });
+    return schema.validate(req);
+}
+
+module.exports = router;
