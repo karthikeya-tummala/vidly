f4af1356d07d9e26f86c5d74ab9d90df0dab964f
karthikeya-tummala <karthikeyatummala@gmail.com>
2024-12-24 18:46:26 +0530

    Removed get methods for users route

--- routes/users.js
+++ routes/users.js
@@ -5,21 +5,6 @@ const argon2 = require('argon2');
 const { validate, User } = require('../models/user');
 const validateId = require('../utils/validateId');
 
-router.get('/', async (req, res) => {
-    const users = await User.find().sort();
-    if(!users) return res.status(404).send('No users in the database');
-    res.send(users);
-});
-
-router.get('/:id', async (req, res) => {
-    const errorId = validateId(req.params.id);
-    if(errorId) return res.status(400).send('Invalid User ID');
-
-    const user = await User.findById(req.params.id);
-    if(!user) return res.status(404).send('No user found for the given ID');
-    res.send(user);
-});
-
 router.post('/', async (req, res) => {
     const { error } = validate(req.body);
     if(error) return res.status(400).send(error.detail[0].message);
